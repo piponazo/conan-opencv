b586075ac5d964ceb3ff86c5e66ab505add0cb88
Luis Díaz Más <piponazo@gmail.com>
2017-10-11 17:46:49 +0200

    Add missing file for travis

---
+++ build.py
@@ -0,0 +1,8 @@
+from conan.packager import ConanMultiPackager
+
+
+if __name__ == "__main__":
+    builder = ConanMultiPackager(username="pix4d", channel="testing",
+                                 upload="https://api.bintray.com/conan/pix4d/conan")
+    builder.add_common_builds()
+    builder.run()
