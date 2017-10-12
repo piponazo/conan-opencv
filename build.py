58f93541efd58726fb605ae48bce716d3a4a1532
Luis Díaz Más <piponazo@gmail.com>
2017-10-12 16:30:57 +0200

    Point to the right bintray repo

--- build.py
+++ build.py
@@ -2,7 +2,7 @@ from conan.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(username="pix4d", channel="testing",
-                                 upload="https://api.bintray.com/conan/pix4d/conan")
+    builder = ConanMultiPackager(username="piponazo", channel="testing",
+                                 upload="https://api.bintray.com/conan/piponazo/piponazo")
     builder.add_common_builds()
     builder.run()
